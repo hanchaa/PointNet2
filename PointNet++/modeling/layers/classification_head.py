cda2cbbfdda7ad2652b53a8e2f5f7057cc291b80
Juhan <hanchaa@gmail.com>
2022-08-14 23:36:54 +0900

    feat(PointNet++): implement classification layer

---
+++ PointNet++/modeling/layers/classification_head.py
@@ -0,0 +1,28 @@
+from torch import nn
+
+
+class ClassificationHead(nn.Module):
+    def __init__(self, in_dim, hidden_dims, num_class, dropout_rates):
+        super().__init__()
+
+        self.linears = nn.ModuleList()
+        self.bns = nn.ModuleList()
+        self.relu = nn.ReLU()
+        self.dropouts = nn.ModuleList()
+
+        previous_dim = in_dim
+        for dim, rate in zip(hidden_dims, dropout_rates):
+            self.linears.append(nn.Linear(previous_dim, dim))
+            self.bns.append(nn.BatchNorm1d(dim))
+            self.dropouts.append(nn.Dropout(rate))
+            previous_dim = dim
+
+        self.classification = nn.Linear(previous_dim, num_class)
+
+    def forward(self, x):
+        for linear, bn, dropout in zip(self.linears, self.bns, self.dropouts):
+            x = dropout(self.relu(bn(linear(x))))
+
+        x = self.classification(x)
+
+        return x
